7cddf801e622500898506a8a1955a0cabef8fc4a
jukry <94563540+jukry@users.noreply.github.com>
2024-01-08 10:05:24 +0200

    chore: grammar

--- server/middleware/newUserLimiter.js
+++ server/middleware/newUserLimiter.js
@@ -2,7 +2,7 @@ import { rateLimit } from "express-rate-limit"
 const newUserLimiter = rateLimit({
     windowMs: 10 * 60 * 1000,
     max: 5,
-    message: "You cannot create that new users in a row",
+    message: "You cannot create that many new users in a row",
 
     handler: (req, res, next, options) => {
         res.status(options.statusCode).send(options.message)
